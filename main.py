3fae3aad875f5b0c404fa6b201d201ff591ef0ee
srdsoproject <srdsosurproject@gmail.com>
2025-07-29 15:13:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if not st.session_state.logged_in:
                 st.session_state.logged_in = True
                 st.session_state.user = user
                 st.success(f"✅ Welcome, {user['name']}!")
-                st.experimental_rerun()
+                st.rerun()
             else:
                 st.error("❌ Invalid email or password.")
     st.stop()
