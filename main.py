0e40f4077149b15de38704e5256a677f7e63dbe9
srdsoproject <srdsosurproject@gmail.com>
2025-08-05 13:21:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -344,7 +344,7 @@ with tabs[0]:
                 'BLNK', 'SGRE', 'KVK', 'LNP', 'DLGN', 'JTRD', 'MSDG', 'JVA', 'WSD', 'SGLA', 'PVR', 'MLB', 'SEI', 'BTW',
                 'PJR', 'DRSV', 'YSI', 'KMRD', 'DKY', 'MRX', 'OSA', 'HGL', 'LUR']
 
-    footplate_list = ["SUR-DD", "SUR-WADI", "LUR-KWV", "KWV-MRJ", "DD-SUR", "WADI-SUR", "KWV-LUR", "MRJ-KWV"]
+    footplate_list = station_list + ["SUR-DD", "SUR-WADI", "LUR-KWV", "KWV-MRJ", "DD-SUR", "WADI-SUR", "KWV-LUR", "MRJ-KWV"]
 
     gate_list = ['LC-19', 'LC-22A', 'LC-25', 'LC-26', 'LC-27C', 'LC-28', 'LC-30', 'LC-31', 'LC-35', 'LC-37', 'LC-40',
              'LC-41', 'LC-43', 'LC-44', 'LC-45', 'LC-46C', 'LC-54', 'LC-61', 'LC-66', 'LC-74', 'LC-76', 'LC-78',
@@ -353,10 +353,7 @@ with tabs[0]:
              'LC-4', 'LC-42', 'LC-02', 'LC-31', 'LC-5', 'LC-6', 'LC-57', 'LC-62', 'LC-66', 'LC-70', 'LC-39',
              'LC-2/C', 'LC-6/C', 'LC-10', 'LC-11', 'LC-15/C', 'LC-21', 'LC-26-A', 'LC-34', 'LC-36', 'LC-44',
              'LC-47', 'LC-55', 'LC-57', 'LC-59', 'LC-60', 'LC-61']
-    INSPECTION_LOCATIONS = {
-    "FOOTPLATE INSPECTION": footplate_list,
-    "STATION INSPECTION": station_list,
-    "LC GATE INSPECTION": gate_list,}
+
 
 
     df = load_data()
@@ -563,6 +560,7 @@ if st.button("✅ Submit Feedback"):
     st.success(f"✅ Feedback updated for {len(edited_df)} rows in Google Sheet")
 
                
+
 
 
 
