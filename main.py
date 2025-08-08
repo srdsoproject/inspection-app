e3fe3b204389994651525a8a5b0fd6ac2c14513e
srdsoproject <srdsosurproject@gmail.com>
2025-08-08 13:30:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -605,7 +605,7 @@ if not editable_filtered.empty:
             use_container_width=True,
             hide_index=True,
             num_rows="fixed",
-            column_config={"User Feedback/Remark": st.column_config.TextColumn("User Feedback/Remark")
+            column_config={"User Feedback/Remark": st.column_config.TextColumn("User Feedback/Remark"),
                           "Deficiencies Noted": st.column_config.TextColumn(
                     "Deficiencies Noted", multiline=True
                 ),
@@ -678,6 +678,7 @@ if not editable_filtered.empty:
                         st.info("ℹ️ No changes detected to save.")
                 else:
                     st.warning("⚠️ No rows matched for update.")
+
 
 
 
