ba047b96c0d6e82701b156085943e1557e4c11e5
srdsoproject <srdsosurproject@gmail.com>
2025-08-16 12:45:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -444,11 +444,7 @@ with tabs[0]:
     df["Status"] = df["Feedback"].apply(classify_feedback)
     
     # ---------- FILTERS ----------
-    start_date, end_date = st.date_input(
-        "📅 Select Date Range",
-        [df["Date of Inspection"].min(), df["Date of Inspection"].max()],
-        key="view_date_range"
-    )
+
     
     col1, col2 = st.columns(2)
     col1.multiselect("Type of Inspection", VALID_INSPECTIONS, key="view_type_filter")
@@ -888,6 +884,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 
 
 
