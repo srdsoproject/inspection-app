ea974ab6dfe17740a6fd2aee123a83e41891691d
srdsoproject <srdsosurproject@gmail.com>
2025-09-16 12:41:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def classify_feedback(feedback, user_remark=""):
             "noted please", "updated by", "adv to", "counselled the staff", "complied", "checked and found",
             "maintained", "for needful action", "provided at", "in working condition", "is working",
             "found working", "equipment is working", "item is working", "as per plan", "putright", "put right",
-            "operational feasibility", "will be provided", "will be supplied shortly", "advised to ubl", "updated"
+            "operational feasibility", "will be provided", "will be supplied shortly", "advised to ubl", "updated", "letter has been sent", "letter has been given"
         ]
 
         pending_kw = [
@@ -717,6 +717,7 @@ st.markdown("""
 - For Engineering North: Pertains to **Sr.DEN/C**
 - For Mechanical: **Pertains to MECHANICAL**
 """)
+
 
 
 
